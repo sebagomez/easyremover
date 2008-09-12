829241c04f9f43d834a982fc7b9287e0890d2761
sebastiangomezcorrea <sebastiangomezcorrea@e1c59394-806c-11dd-ba2e-09f2ff3e973f>
2008-09-12 02:10:33 +0000

    - trunk created

---
+++ trunk/Entry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Program_Finder
+{
+    class Entry
+    {
+        public Entry(string name)
+        {
+            Name = name;
+        }
+
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Publisher { get; set; }
+        public string Source { get; set; }
+        public string Unisntall { get; set; }
+        public DateTime InstallationDate { get; set; }
+        public Icon LargeIcon { get; set; }
+        public Icon SmallIcon { get; set; }
+        public bool IsUpdate { get; set; }
+        public string Help { get; set; }
+    }
+}
